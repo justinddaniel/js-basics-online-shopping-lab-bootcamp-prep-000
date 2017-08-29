c95a67ccf65211d4c051e649cde15c4f3a7429d8
Justin Daniel <justinddaniel@gmail.com>
2017-08-29 21:32:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  let itemName = item
+  let itemPrice = Math.floor(Math.random() * 100) + 1
+  var itemConstructor = {[itemName]: itemPrice}
+  cart.push(itemConstructor)
+  console.log(`${itemName} has been added to your cart.`)
+  return cart
 }
 
 function viewCart() {
-  // write your code here
-}
+  const yourCart = ['In your cart, you have']
+  for (var i = 0; i < cart.length; i++) {
+    new object1 = cart[i]
+    var keys = Object.keys(object1)
+    var values = object1[keys[i]]}
+    for (var i = 0; i < cart.length; i++) {
+      yourCart.push(` ${keys[i]} at ${values[i]},`)
+    } for (var i = cart.length; i = cart.length; i++) {
+      yourCart.push(` and ${keys[i]} at ${values[i]}.`)
+    } return youCart
+
+
 
 function total() {
   // write your code here
