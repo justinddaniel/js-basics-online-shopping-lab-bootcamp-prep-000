6d9df1b02576144ff8dbde19c11923ac7780dc60
Justin Daniel <justinddaniel@gmail.com>
2017-08-29 22:31:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,8 +65,9 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
-} else {
+  else {
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   var cart = []
   return cart;
+}
 }
