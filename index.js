7383d9a88b967f5cb4fcb959fb8fa5a5588c9fe6
Justin Daniel <justinddaniel@gmail.com>
2017-08-29 21:54:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function viewCart() {
 function total() {
 cart.forEach ((itemObject, index) => {
   var keys2 = Object.keys(cart)[0]
-  var price = itemObject[keys]})
+  var price = itemObject[keys2]})
   var sum = price.reduce((a, b) => a + b, 0);
   console.log(sum);
 }
