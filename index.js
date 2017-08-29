51bda8a05a6e3a1bdc1c6605211b82988d945ca3
Justin Daniel <justinddaniel@gmail.com>
2017-08-29 22:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
   }
   else {
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-  var cart = []
+  cart = []
   return cart;
 }
 }
